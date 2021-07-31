8f6d77f6e6b94a5557ffb9328c9ff4767ca49d5c
Skyler Harris <skyler.iqeq@gmail.com>
2021-07-31 11:07:36 +0800

    first attempt

--- build_a_quiz_challenge/js/quiz.js
+++ build_a_quiz_challenge/js/quiz.js
@@ -1,6 +1,11 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
 
 
+const quiz = [
+  ['Who is Skys dog?', 'Who is Cynthias dog?', 'Who are Tricia and Justins dogs?']
+  ['Zara', 'Max', 'Denver and Tucker']
+]
+
 // 2. Store the number of questions answered correctly
 
 
@@ -11,6 +16,15 @@
       - If the response matches the answer, the number of correctly
         answered questions increments by 1
 */
+
+function questions ( arr ) {
+  let questionsCorrect = '';
+  for ( let i = 0; i < arr.length; i++ ) {
+    questionsCorrect += `${ arr[i] }`;
+  }
+      return questionsCorrect;
+  }
+
 
 
 // 4. Display the number of correct answers to the user
