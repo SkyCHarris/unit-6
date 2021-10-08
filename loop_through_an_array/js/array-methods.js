9df318a41db50e67cb1819eca495e706f4e068cd
Sky Harris <skyler.iqeq@gmail.com>
2021-10-08 14:08:03 +0800

    array methods arrays

---
+++ loop_through_an_array/js/array-methods.js
@@ -0,0 +1,17 @@
+const daysInWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+];
+
+const fruit = [
+    'apple',
+    'orange',
+    'grapefruit',
+    'pineapple',
+    'strawberry'
+]
