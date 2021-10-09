a3af3cda29e2fb8e6723ed29d70d7add5060c665
Sky Harris <skyler.iqeq@gmail.com>
2021-10-09 10:49:13 +0800

    convert user entry to lowercase

--- loop_through_an_array/js/search-list.js
+++ loop_through_an_array/js/search-list.js
@@ -6,8 +6,8 @@ if ( !search ) {
     message = `<strong>In stock:</strong> ${inStock.join(', ')}`;
 }
 
-else if (inStock.includes(search)){
-    message = `Yes, we have <strong>${search}</strong>. It's #${inStock.indexOf(search) + 1} on the list!`;
+else if (inStock.includes(search.toLowerCase())){
+    message = `Yes, we have <strong>${search}</strong>. It's #${inStock.indexOf(search.toLowerCase) + 1} on the list!`;
 } else {
     message = `Sorry, we do not have <strong>${search}</strong>`;
 }
