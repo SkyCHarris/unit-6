d974bfc32b80d384fcd183f23de914c470ee0152
Sky Harris <skyler.iqeq@gmail.com>
2021-10-11 15:23:43 +0800

    playlist array updated to multidimensional array

--- multidimensional_array/js/create_a_multidimensional_array.js
+++ multidimensional_array/js/create_a_multidimensional_array.js
@@ -1,12 +1,13 @@
 
 const playlist = [
-    'So What',
-    'Respect',
-    'What a Wonderful World',
-    'At Last',
-    'Three Little Birds',
-    'The Way You Look Tonight'
+    ['So what', 'Miles Davis', '9:04']
+    ['Respect', 'Aretha Franklin', '2:45']
+    ['What a Wonderful World', 'Louis Armstrong', '2:21'],
+    ['At Last', 'Ella Fitzgerald', '4:18'],
+    ['Three Little Birds', 'Bob Marley and the Wailers', '3:01'],
+    ['The Way You Look Tonight', 'Frank Sinatra', '3:21']
   ];
+
 
 
 function createListItems(arr) {
