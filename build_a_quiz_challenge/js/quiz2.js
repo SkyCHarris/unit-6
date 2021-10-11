eaa2f1cf5df308b59849eb6dd6d1297ff5185f46
Sky Harris <skyler.iqeq@gmail.com>
2021-10-11 17:00:12 +0800

    multidimensional array and variables for build a quiz challenge

---
+++ build_a_quiz_challenge/js/quiz2.js
@@ -0,0 +1,18 @@
+
+// Array
+const cynthiaQuiz = [
+    ['Who is the prettiest girl in the world?', 'Cynthia.'],
+    ['What makes her the prettiest girl in the world?', 'Her huge eyes and tremendous personality.'],
+    ['What are we gonna do about it?', 'Try to make her our wifey.']
+]
+
+// Store Questions in Variables
+const whoPretty =  cynthiaQuiz[0];
+const whyPretty = cynthiaQuiz[1];
+const whatGonDoBoutIt = cynthiaQuiz[2];
+
+
+// If else to 
+if ( whoPretty[0][1] == "Cynthia" ) {
+    
+}
