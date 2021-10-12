d41caa1ee5677fd3f160bc81f62f954d5a58d4df
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-12 12:54:50 +0800

    update to array question answers

--- build_a_quiz_challenge/js/quiz2.js
+++ build_a_quiz_challenge/js/quiz2.js
@@ -2,17 +2,23 @@
 // Array
 const cynthiaQuiz = [
     ['Who is the prettiest girl in the world?', 'Cynthia.'],
-    ['What makes her the prettiest girl in the world?', 'Her huge eyes and tremendous personality.'],
-    ['What are we gonna do about it?', 'Try to make her our wifey.']
+    ['What makes her the prettiest girl in the world?', 'Everything.'],
+    ['What are we gonna do about it?', 'Wife it up.']
 ]
 
-// Store Questions in Variables
-const whoPretty =  cynthiaQuiz[0];
-const whyPretty = cynthiaQuiz[1];
-const whatGonDoBoutIt = cynthiaQuiz[2];
 
 
 // If else to 
-if ( whoPretty[0][1] == "Cynthia" ) {
-    
+
+const whoPretty = prompt("Who's the prettiest girl in the world?");
+const whyPretty = prompt("Everything")
+
+if (whoPretty = 'Cynthia') {
+    i++
 }
+
+
+
+
+// For Loop
+for ( let i = 0; i <)
