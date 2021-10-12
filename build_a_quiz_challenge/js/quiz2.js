b2800659b8cc14630ea7f04b3bb25cb5746b4f83
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-12 13:37:09 +0800

    Updated for loop, but problems persists

--- build_a_quiz_challenge/js/quiz2.js
+++ build_a_quiz_challenge/js/quiz2.js
@@ -10,15 +10,35 @@ const cynthiaQuiz = [
 
 // If else to 
 
-const whoPretty = prompt("Who's the prettiest girl in the world?");
-const whyPretty = prompt("Everything")
+// const whoPretty = prompt("Who's the prettiest girl in the world?");
+// const whyPretty = prompt("What makes her the prettiest girl in the world?")
+// const whatDo = prompt("What are we gonna do about it?")
 
-if (whoPretty = 'Cynthia') {
-    i++
+
+
+for ( let i = 0; i < cynthiaQuiz.length; i++) {
+    let score = 0;
+    const whoPretty = prompt(cynthiaQuiz[0][0]);
+    if ( whoPretty == 'Cynthia') {
+        score++;
+    } else {
+        score;
+    }
+    console.log(score);
 }
 
 
+// function correctAnswers() {
+//     for (let i = 0; i < cynthiaQuiz.length; i++) {
+//         const prettyQuestions = prompt(i[0]);
+//     }
+    
+// }
 
 
-// For Loop
-for ( let i = 0; i <)
+// Use loop to cycle through each question, 
+// present it to the user,
+// and compare the response from the user to the answer in the array
+
+// Use prompt method to ask questions, 
+// and conditional statement to check if user answer matches the correct answer
