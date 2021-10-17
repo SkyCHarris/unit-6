24ea700bd5642308db4d37b5f6df508e7c5de61e
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-17 20:30:25 +0800

    build a quiz challenge solution

--- build_a_quiz_challenge/js/quiz2.js
+++ build_a_quiz_challenge/js/quiz2.js
@@ -27,59 +27,21 @@ for ( let i = 0; i < cynthiaQuiz.length; i++ ) {
     }
 }
 
+function createListItems(arr) {
+    let items = '';
+    for ( let i = 0; i < arr.length; i++) {
+        items += `<li>${arr[i]}</li>`;
+    } 
+    return items;
+}
+
 let html = `
     <h1>You got ${score} question(s) correct!</h1>
+    <h2>You got these questions right:</h2>
+    <ol>${ createListItems(correctQuestions) }</ol>
+
+    <h2>You got these questions wrong:</h2>
+    <ol>${ createListItems(incorrectQuestions) }</ol>
 `
 
 document.querySelector('main').innerHTML = html;
-
-
-let questionsList = `
-    <h1>You got these questions right:</h1>
-    <ol>
-        <li>${correctQuestions}</li>
-    </ol>
-    <h1>You got these questions wrong:</h1>
-    <ol>
-        <li>${incorrectQuestions}</li>
-    </ol>
-`   
-
-document.querySelector('main').innerHTML = questionsList;
-
-
-
-
-// Use loop to cycle through each question, 
-// present it to the user,
-// and compare the response from the user to the answer in the array
-
-// Use prompt method to ask questions, 
-// and conditional statement to check if user answer matches the correct answer
-
-
-// Practice
-
-// const colorsQuiz = [
-//     ['What color is the sky?', 'Blue'],
-//     ['What color are trees?', 'Green'],
-//     ['What color is the sun?', 'Yellow']
-// ]
-
-// let score = 0;
-
-// for ( let i = 0; i < colorsQuiz.length; i++ ) {
-//     let quizQuestions = colorsQuiz[i][0];
-//     let quizAnswers = colorsQuiz[i][1];
-//     let quizResponse = prompt(colorsQuiz);
-// }
-
-// if ( quizResponse = quizAnswers) {
-//     score++;
-//     console.log(score);
-// }
-
-// html = 
-// `<h1>You got ${score} question(s) correct!</h1>`
-
-// document.querySelector('main').innerHTML = html;
