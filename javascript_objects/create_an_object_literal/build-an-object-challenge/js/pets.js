5d847296b239ea4d9d817aab8601075fd3e5280d
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-18 13:57:15 +0800

    pets object array

--- javascript_objects/create_an_object_literal/build-an-object-challenge/js/pets.js
+++ javascript_objects/create_an_object_literal/build-an-object-challenge/js/pets.js
@@ -3,3 +3,43 @@
   Each object should have the following properties: 
   name, type, breed, age, and photo
 */
+
+const pets = [
+  { name: 'Oz',
+    type: 'dog',
+    breed: 'aussie',
+    age: 6,
+    photo: 'aussie.jpg'
+  },
+  { name: 'Dashing',
+    type: 'dog',
+    breed: 'dachshund',
+    age: 2,
+    photo: 'daschund.jpg'
+  },
+  { name: 'Goldie',
+    type: 'dog',
+    breed: 'golden',
+    age: 10,
+    photo: 'golden.jpg'
+  },
+  { name: 'Perry',
+    type: 'cat',
+    breed: 'persian',
+    age: 1,
+    photo: 'persian.jpg'
+  },
+  { name: 'Bruce',
+    type: 'dog',
+    breed: 'pug',
+    age: 6,
+    photo: 'pug.jpg'
+  },
+  { name: 'Tabatha',
+    type: 'cat',
+    breed: 'tabby',
+    age: 7,
+    photo: 'tabby.jpg'
+  },
+
+]
