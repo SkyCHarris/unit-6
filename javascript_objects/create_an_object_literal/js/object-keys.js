4c55157c94c1a800059eeb1515b1a4a950001ac7
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-18 11:22:27 +0800

    object-keys

---
+++ javascript_objects/create_an_object_literal/js/object-keys.js
@@ -0,0 +1,14 @@
+
+
+// JavaScript Object
+const person = {
+    name: 'Reggie',
+    role: 'Software developer',
+    skills: ['JavaScript', 'HTML', 'CSS'],
+    isTeacher: true
+};
+
+// Store the keys of the 'person' object in 'personProps'
+
+const personProps = Object.keys(person);
+console.log(personProps);
