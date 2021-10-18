fc2fac3fab6a59423bc0feff1bb94d58368691ca
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-18 15:35:33 +0800

    build an object challenge first attempt

--- javascript_objects/create_an_object_literal/build-an-object-challenge/js/pets.js
+++ javascript_objects/create_an_object_literal/build-an-object-challenge/js/pets.js
@@ -6,40 +6,58 @@
 
 const pets = [
   { name: 'Oz',
-    type: 'dog',
+    type: 'Dog',
     breed: 'aussie',
     age: 6,
-    photo: 'aussie.jpg'
+    photo: 'img/aussie.jpg'
   },
   { name: 'Dashing',
-    type: 'dog',
+    type: 'Dog',
     breed: 'dachshund',
     age: 2,
-    photo: 'daschund.jpg'
+    photo: 'img/daschund.jpg'
   },
   { name: 'Goldie',
-    type: 'dog',
+    type: 'Dog',
     breed: 'golden',
     age: 10,
-    photo: 'golden.jpg'
+    photo: 'img/golden.jpg'
   },
   { name: 'Perry',
-    type: 'cat',
+    type: 'Cat',
     breed: 'persian',
     age: 1,
-    photo: 'persian.jpg'
+    photo: 'img/persian.jpg'
   },
   { name: 'Bruce',
-    type: 'dog',
+    type: 'Dog',
     breed: 'pug',
     age: 6,
-    photo: 'pug.jpg'
+    photo: 'img/pug.jpg'
   },
   { name: 'Tabatha',
-    type: 'cat',
+    type: 'Cat',
     breed: 'tabby',
     age: 7,
-    photo: 'tabby.jpg'
+    photo: 'img/tabby.jpg'
   },
-
 ]
+
+for ( let i = 0; i < pets.length; i++ ) {
+  let pet = pets[i];
+  let name = pets[i].name;
+  let type = pets[i].type;
+  let breed = pets[i].breed;
+  let age = pets[i].age;
+  let photo = pets[i].photo;
+}
+
+
+let HTML = `
+<h2>${name}</h2>
+<h3>${type} | ${breed}</h3>
+<p>${age}</p>
+<img src=${photo} alt=${breed}
+`
+
+document.querySelector('main').innterHTML = HTML;
