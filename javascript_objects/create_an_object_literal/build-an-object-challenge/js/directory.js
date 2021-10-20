bfca35f6a3c89d75a017b727428aa362ba4baa63
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-20 22:52:25 +0800

    build an object challenge reminder practice

--- javascript_objects/create_an_object_literal/build-an-object-challenge/js/directory.js
+++ javascript_objects/create_an_object_literal/build-an-object-challenge/js/directory.js
@@ -1,16 +1,31 @@
 
 let html = '';
 
+// for ( let i = 0; i < pets.length; i++ ) {
+//   let pet = pets[i];
+//   html += `
+//   <h2>${pet.name}</h2>
+//   <h3>${pet.type} | ${pet.breed}
+//   <p>${pet.age}</p>
+//   <img src =${pet.photo} alt=${pet.breed}>
+//   ` 
+// };
+
+// console.log(html);
+
+// document.querySelector('main').innerHTML = html;
+
+
+
+
+
+// Practice / Reminder
 for ( let i = 0; i < pets.length; i++ ) {
-  let pet = pets[i];
+  let html = '';
   html += `
-  <h2>${pet.name}</h2>
-  <h3>${pet.type} | ${pet.breed}
-  <p>${pet.age}</p>
-  <img src =${pet.photo} alt=${pet.breed}>
-  ` 
-};
-
-console.log(html);
-
-document.querySelector('main').innerHTML = html;
+  <h2>${pets[i].name}</h2>
+  <h3>${pets[i].type} | ${pets[i].breed}</h3>
+  <p>${pets[i].age}</p>
+  <img src=${pets[i].photo}>
+  `
+}
