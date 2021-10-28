c87c9c4da627744fc7403d84a68334c8f2c3f9e4
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-28 12:41:43 +0800

    get list items, assign listItems variable to html collection of list items

--- javascript_and_the_dom/the_browsers_global_window_object/app.js
+++ javascript_and_the_dom/the_browsers_global_window_object/app.js
@@ -1,7 +1,12 @@
 const headline = document.getElementById('headline');
 const buttonStyling = document.getElementById('btn-main');
+const listItems = document.getElementsByTagName('li');
+
+
+
 
 
 buttonStyling.addEventListener('click', () => {
     headline.style.border = 'solid 2px red';
+    headline.style.fontSize = '60px';
 });
