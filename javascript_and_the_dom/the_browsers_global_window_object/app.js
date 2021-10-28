3d4946d95a8c99f66bcf7c5236790fd70d90738f
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-28 12:46:37 +0800

    for loop to iterate through list items, change each to orchid text color

--- javascript_and_the_dom/the_browsers_global_window_object/app.js
+++ javascript_and_the_dom/the_browsers_global_window_object/app.js
@@ -1,10 +1,11 @@
 const headline = document.getElementById('headline');
 const buttonStyling = document.getElementById('btn-main');
+
 const listItems = document.getElementsByTagName('li');
 
-
-
-
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].style.color = 'orchid';
+}
 
 buttonStyling.addEventListener('click', () => {
     headline.style.border = 'solid 2px red';
