5283e229bb747477aea5576921c63dfbddf00b9b
Skyler Harris <skyler.iqeq@gmail.com>
2021-10-28 10:39:43 +0800

    add addEventListener to style headline on click

--- javascript_and_the_dom/the_browsers_global_window_object/app.js
+++ javascript_and_the_dom/the_browsers_global_window_object/app.js
@@ -1,3 +1,6 @@
 const headline = document.getElementById('headline');
 
-headline.style.border = 'solid 2px red';
+
+headline.addEventListener('click', () => {
+    headline.style.border = 'solid 2px red';
+});
