a1d48f980b63f37b843581dc3e63066fedc78705
Skyler Harris <skyler.iqeq@gmail.com>
2021-12-15 00:24:41 +0800

    update app.js file for creating new DOM elements

--- javascript_and_the_dom/the_browsers_global_window_object/app.js
+++ javascript_and_the_dom/the_browsers_global_window_object/app.js
@@ -1,32 +1,17 @@
 
 const btnToggle = document.querySelector('.btn-toggle');
-const btnUpdate = document.querySelector('.btn-main');
-const buttonStyling = document.querySelector('.btn-main');
-// const listItems = document.querySelectorAll('li');
-// const highlights = document.querySelectorAll('.highlight');
+const btnCreate = document.querySelector('.btn-main');
 
-
-// for ( const highlight of highlights) {
-//     highlight.style.backgroundColor = 'cornsilk';
-
-//     console.log(highlight);
-// }
-
-
-// for (let i = 0; i < listItems.length; i++) {
-//     listItems[i].style.color = 'orchid';
-// }
 
 buttonStyling.addEventListener('click', () => {
-    const headline = document.getElementById('headline');
     const input = document.querySelector('.input-main');
     
-    headline.className = 'grow';
-    headline.textContent = input.value;
+    
     input.value = '';
 });
 
-btnToggle.addEventListener('click', () => {
+
+btnCreate.addEventListener('click', () => {
     const listContainer = document.querySelector('.list-container');
 
     if (listContainer.style.display === 'none') {
