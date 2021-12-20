fa03e3d7498fde3a210cb90f196bfe8bcfa3fd0a
Skyler Harris <skyler.iqeq@gmail.com>
2021-12-20 15:12:49 +0800

    select all h2 elements and change color, and, select all card class elemeents and change background color

--- practice_JS_and_the_DOM/js/scripts.js
+++ practice_JS_and_the_DOM/js/scripts.js
@@ -9,20 +9,32 @@ about.style.border = "2px solid firebrick";
 // 2: Select all the <h2> elements in the document.
 //    Set the color of the <h2> elements to a different color.
 
+const headers = document.getElementsByTagName('h2');
+
+for (let i = 0; i < headers.length; i++) {
+    headers[i].style.color = 'darkblue';
+}
+
 
 // 3: Select all elements with the class '.card'. 
 //    Set their background color to the color of your choice.
 
 
+const cards = document.getElementsByClassName('card');
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].style.backgroundColor = 'lightgray';
+}
+
 // 4: Select only the first <ul> in the document.
 //    Assign it to a variable named `ul`.
 
-ul.style.border = "2px solid indigo";
+// ul.style.border = "2px solid indigo";
 
 // 5: Select only the second element with the class '.container'.
 //    Assign it to a variable named `container`.
 
-container.style.backgroundColor = "royalblue";
+// container.style.backgroundColor = "royalblue";
 
 // 6: Select all <a> elements that have a 'title' attribute. 
 //    Set their color value to the color of your choice.
