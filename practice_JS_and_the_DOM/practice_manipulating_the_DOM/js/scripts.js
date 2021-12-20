366354b3dbb9c4c8ffa9762b6118020dff71dbb4
Skyler Harris <skyler.iqeq@gmail.com>
2021-12-20 18:16:04 +0800

    attempts on 1-5

--- practice_JS_and_the_DOM/practice_manipulating_the_DOM/js/scripts.js
+++ practice_JS_and_the_DOM/practice_manipulating_the_DOM/js/scripts.js
@@ -5,23 +5,25 @@ document.querySelector('h1').textContent = 'My To-Do List';
 
 // 2: Set the color of the <h1> to a different color
 
-
+document.querySelector('h1').style.color = "darkorange";
 
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
 
-
+const paragraph = document.getElementsByClassName('desc');
+paragraph.innerHTML = "This is my To-Do List! Take a look, cuz Im soooo busy";
 
 
 // 4: Set the class of the <ul> to 'list'
 
-
-
+const list = document.querySelector('ul');
+list.className = 'list';
 
 // 5: Create a new list item and add it to the <ul>
 
-
+const ul  = document.querySelector('ul');
+const newListItem = document.createElement('li');
 
 
 // 6: Change all <input> elements from text fields to checkboxes
